b0c644d8fb2bd7afb52d83d3a4905d33e26daf77
User <paulate.alonso@gmail.com>
2022-11-05 11:03:44 +0100

    Ex7

--- EXERCISE 1/main.js
+++ EXERCISE 1/main.js
@@ -1,4 +1,4 @@
-function arr(numb){
+/* function arr(numb){
     let suma = 0
     for (let i=1; i<x; i++){
       if (i%2 !== 0) {
@@ -6,7 +6,30 @@ function arr(numb){
       }
     }
     return suma
-  }
+  } */
   
 // console.log(arr(4));
 
+
+function oddSum(arr) {
+  let counter = 0
+
+  for (let i = 0; i<arr.length; i++){
+    if (arr[i]%2 === 1){
+      counter += arr[i]
+    }
+  }
+  return counter
+}
+
+// const oddSum = (arr) => {
+//   let counter = 0
+//   arr.forEach(e =>{
+//     if (e%2 === 1){
+//       counter += e
+//     }
+//   })
+//   return counter
+// }
+
+console.log(oddSum([45, 3, 2, 5, 6, 100]))
--- EXERCISE 2/main.js
+++ EXERCISE 2/main.js
@@ -1,5 +1,21 @@
-let arr = ['uno', 'dos', 'tres', 'cuatro'];
+/* let arr = ['uno', 'dos', 'tres', 'cuatro'];
 arr.forEach(function(ar) {
     ar.reverse();
     console.log(ar); 
-});
+}); */
+
+function arrPrint(arr){
+    for (let i = arr.length-1; i>=0; i--){
+      console.log(arr[i])
+    }
+    return
+  }
+  
+  // const arrPrint = (arr) =>{
+  //   arr.reverse().forEach(e=>{
+  //     console.log(e);
+  //   })
+  // }
+  
+  
+  arrPrint(['whisky', 'blanco', 'keto', 'teot', 'yuju'])
